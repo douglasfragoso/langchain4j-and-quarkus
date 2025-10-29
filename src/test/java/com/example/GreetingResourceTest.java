a8f873008de6e6387bb59a78340a63968e4834b4
Douglas Fragoso <douglas.iff@gmail.com>
2025-10-28 21:19:44 -0300

    Add Quarkus project with LangChain4j and Java 25

---
+++ src/test/java/com/example/GreetingResourceTest.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello from Quarkus REST"));
+    }
+
+}
+
